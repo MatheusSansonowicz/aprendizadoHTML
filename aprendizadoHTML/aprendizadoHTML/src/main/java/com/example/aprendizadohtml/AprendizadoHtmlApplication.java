dee4c0692f43ae37ed00f2a6edcef648ee012c93
Matheus Sansonowicz <128191037+MatheusSansonowicz@users.noreply.github.com>
2025-05-30 18:28:36 -0300

    init comit

---
+++ aprendizadoHTML/aprendizadoHTML/src/main/java/com/example/aprendizadohtml/AprendizadoHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.aprendizadohtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendizadoHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AprendizadoHtmlApplication.class, args);
+    }
+
+}
---
+++ aprendizadoHTML/aprendizadoHTML/src/test/java/com/example/aprendizadohtml/AprendizadoHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aprendizadohtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendizadoHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
